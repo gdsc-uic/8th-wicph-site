ada8454470574893580fdf5a286cad0830162a57
Ned Palacios <npdoesmc@gmail.com>
2022-08-04 22:07:47 +0800

    add google forms

--- pages/registration.vue
+++ pages/registration.vue
@@ -2,7 +2,9 @@
 <div>
     <NuxtLayout name="page" title="Registration">
         <div class="container py-12">
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfmGo-IlpzIS2F74sttXUblPov4C3GWDAdSkLuZ5f8aQrWpWw/viewform" class="underline text-secondary-400">Registration Link</a>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfmGo-IlpzIS2F74sttXUblPov4C3GWDAdSkLuZ5f8aQrWpWw/viewform?embedded=true" style="width: 100%;" height="1751" frameborder="0" marginheight="0" marginwidth="0">
+                Loading…
+            </iframe>
         </div>
     </NuxtLayout>
 </div>
