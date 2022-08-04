8553ffb0e147380d292d0021f5265af720f82927
Ned Palacios <npdoesmc@gmail.com>
2022-08-04 22:18:05 +0800

    revert ssr false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,6 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    ssr: false,
-    nitro: {
-        preset: 'service-worker'
-    },
     modules: [
         '@nuxt/content',
         'nuxt-windicss',
