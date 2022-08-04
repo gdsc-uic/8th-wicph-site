67fd025a07da7dba286a79a724c0700335705cf6
Ned Palacios <npdoesmc@gmail.com>
2022-08-04 21:39:10 +0800

    add link to gform

--- pages/registration.vue
+++ pages/registration.vue
@@ -1,7 +1,9 @@
 <template>
 <div>
     <NuxtLayout name="page" title="Registration">
-
+        <div class="container py-12">
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfmGo-IlpzIS2F74sttXUblPov4C3GWDAdSkLuZ5f8aQrWpWw/viewform" class="underline text-secondary-400">Registration Link</a>
+        </div>
     </NuxtLayout>
 </div>
 </template>
