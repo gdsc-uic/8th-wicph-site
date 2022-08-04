bca0e07672a066769871bc0d3655406165d62b55
Ned Palacios <npdoesmc@gmail.com>
2022-08-04 22:07:42 +0800

    make it static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    ssr: false,
+    nitro: {
+        preset: 'service-worker'
+    },
     modules: [
         '@nuxt/content',
         'nuxt-windicss',
