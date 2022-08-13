273be85facdb54fce2ede740f800b0cf39d8ed0c
Ned Palacios <npdoesmc@gmail.com>
2022-08-14 00:26:26 +0800

    improve nav for now

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,10 @@
 <template>
     <header class="bg-primary-400 text-secondary-400">
-        <div class="pt-4 container">
+        <!-- TODO: improve nav -->
+        <div class="pt-4 container <md:px-0">
             <a href="/">WiCPH2022</a>
             
-            <div class="pt-4 ml-auto flex items-center space-x-4 font-semibold border-b border-secondary-400">
+            <div class="<md:max-w-screen w-full overflow-x-scroll pt-4 flex items-center space-x-4 font-semibold border-b border-secondary-400">
                 <NuxtLink to="/">Home</NuxtLink>
                 <NuxtLink to="/committees">Committees</NuxtLink>
                 <NuxtLink to="/submission">Submission</NuxtLink>
