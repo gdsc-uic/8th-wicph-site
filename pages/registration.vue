c67c968e560e2beec0c920d880ae2fbf52b8dd53
Ned Palacios <npdoesmc@gmail.com>
2022-08-25 18:00:29 +0800

    add alternative link banner for registration if iframe fails

--- pages/registration.vue
+++ pages/registration.vue
@@ -1,8 +1,13 @@
 <template>
 <div>
     <NuxtLayout name="page" title="Registration">
+        
         <div class="container py-12">
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfmGo-IlpzIS2F74sttXUblPov4C3GWDAdSkLuZ5f8aQrWpWw/viewform?embedded=true" style="width: 100%;" height="1751" frameborder="0" marginheight="0" marginwidth="0">
+            <div class="border border-primary-400 bg-primary-50 p-4 rounded-lg mb-8">
+                <p><b>Cannot access the form?</b> You may try again register by clicking to this <a class="underline text-secondary-800" :href="`${formLink}?usp=sf_link`" target="_blank">link</a>.</p>
+            </div>
+
+            <iframe :src="`${formLink}?embedded=true`" style="width: 100%;" height="1751" frameborder="0" marginheight="0" marginwidth="0">
                 Loading…
             </iframe>
         </div>
@@ -11,6 +16,8 @@
 </template>
 
 <script setup lang="ts">
+const formLink = "https://docs.google.com/forms/d/e/1FAIpQLSfmGo-IlpzIS2F74sttXUblPov4C3GWDAdSkLuZ5f8aQrWpWw/viewform"
+
 definePageMeta({
     layout: false
 });
