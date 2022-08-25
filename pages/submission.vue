b2c42e76efee0451a685661b32fbe07e68b70145
Ned Palacios <npdoesmc@gmail.com>
2022-08-25 18:00:14 +0800

    improve submission content styling

--- pages/submission.vue
+++ pages/submission.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
     <NuxtLayout name="page" title="Submission">
-        <div class="max-w-2xl mx-auto w-full px-4 prose pb-12">
+        <div class="submission-content max-w-2xl mx-auto w-full px-4 prose-lg pb-12">
             <ContentRenderer :value="data">
                 <ContentRendererMarkdown :value="data" />
             </ContentRenderer>
@@ -17,3 +17,9 @@ definePageMeta({
 
 const { data } = await useAsyncData('submission', () => queryContent('/submission_guidelines').findOne());
 </script>
+
+<style lang="postcss">
+.submission-content h2 a {
+    @apply !no-underline !font-bold !text-secondary-800;
+}
+</style>
