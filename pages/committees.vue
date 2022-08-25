b5f3a19dc53d8ddae6ab7a69ec313d9417f75998
Ned Palacios <npdoesmc@gmail.com>
2022-08-25 18:01:12 +0800

    improve contrast for commitee texts

--- pages/committees.vue
+++ pages/committees.vue
@@ -3,7 +3,7 @@
     <NuxtLayout name="page" title="Committees">
       <div class="container flex flex-col py-8 space-y-8">
         <div :key="c.committee" v-for="c in data.body">
-            <h2 class="text-2xl font-bold text-secondary-400 border-b pb-3">{{ c.committee }}</h2>
+            <h2 class="text-2xl font-bold text-secondary-700 border-b pb-3">{{ c.committee }}</h2>
 
             <template v-for="(label, key) in chairsKV" :key="`${c.committee}_${label}`">
               <template v-if="c[key]">
@@ -18,7 +18,7 @@
                       <div v-else class="h-16 w-16 bg-gray-200 rounded-full"></div>
                       <div class="flex-1 pl-2 flex flex-col justify-center">
                         <h3 class="font-semibold">{{ p.name }}</h3>
-                        <p class="text-gray-500 text-sm">{{ p.institution }}</p>
+                        <p class="text-gray-800 text-sm">{{ p.institution }}</p>
                       </div>
                     </div>
                   </div>
